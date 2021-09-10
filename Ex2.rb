5b2c28b3ed8edebe26c38af270803dbbd7f3dc56
Luigi <79731082+LuigiPWOliveira@users.noreply.github.com>
2021-09-10 04:05:55 -0300

    Update Ex2.rb

--- Ex2.rb
+++ Ex2.rb
@@ -1,1 +1,20 @@
+include Math
+
+def equacao2grau(a, b, c)
+
+    delta = (b*b) - (4*a*c)
+    puts Math.sqrt(delta)
+    x1 = ( - b + Math.sqrt(delta))/(2*a)
+    x2 = ( - b - Math.sqrt(delta))/(2*a)
+
+    if(delta >= 0)
+        if(x1 == x2)
+            puts x1
+        else
+            puts "[" + x1.to_s + ", " + x2.to_s + "]"
+        end
+    else
+        return false
+    end
+end
 
